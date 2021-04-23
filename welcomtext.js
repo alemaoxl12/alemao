bb1ca477dcc73d04452f408915677480dedd887e
alemaoxl12 <nozeboladao1@gmail.com>
2021-04-23 10:20:45 -0300

    First commit

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. Bem vindo seu mito ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. Já vai tarde 👋`
+}
+exports.bye = bye
